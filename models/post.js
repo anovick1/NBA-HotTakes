546aefd621052b9952fad14096eeed63d3b125ae
Avery Novick <averynov@yahoo.com>
2022-07-25 09:48:01 -0400

    post schema

---
+++ models/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema(
+  {
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String, required: true },
+    text: { type: String, required: true },
+    image: { type: String, required: true },
+    comments: { type: String, required: true },
+    likes: { type: String, required: true },
+    dislikes: { type: String, required: true }
+  },
+  { timestamps: true }
+)
+
+module.exports = reviewSchema
