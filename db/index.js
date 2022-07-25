c91e0edfc528e0c28844a8b60c35498eeb45f908
Avery Novick <averynov@yahoo.com>
2022-07-25 11:14:11 -0400

    data

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/takesDatabase'
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/postsDatabase'
 
 mongoose
   .connect(MONGODB_URI)
